03998841868336269a8497cbe2aa1dadd55f7727
ImLJS <109210868+ImLJS@users.noreply.github.com>
2025-01-06 07:04:28 +0530

    [feat] - Add Constants

---
+++ src/constants/sidebar-menu.ts
@@ -0,0 +1,33 @@
+import { CircleUserRound, House, LifeBuoy, Music, Send } from 'lucide-react';
+
+export const data = {
+  support: [
+    {
+      title: 'Support',
+      url: '#',
+      icon: LifeBuoy,
+    },
+    {
+      title: 'Feedback',
+      url: '#',
+      icon: Send,
+    },
+  ],
+  projects: [
+    {
+      name: 'Home',
+      url: '#',
+      icon: House,
+    },
+    {
+      name: 'Tracks',
+      url: '#',
+      icon: Music,
+    },
+    {
+      name: 'Artists',
+      url: '#',
+      icon: CircleUserRound,
+    },
+  ],
+};
